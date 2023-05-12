f9697db48dfc70dc3bc6f6d650c92815b335839b
Aryan Rathi <aryan2001rathi@gmail.com>
2023-05-12 10:49:22 +0530

    fact_app based on YT second commit

--- app/src/main/java/com/example/fact_app/MainActivity.java
+++ app/src/main/java/com/example/fact_app/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.fact_app;
 
 
-
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     TextView factTextView;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
